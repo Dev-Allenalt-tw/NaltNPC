9f63c150ae3e2dbac18b709f77ea3fd99d3b67fe
Dev-Allenalt-tw <rohitcreationsyt@gmail.com>
2025-11-03 12:02:06 +0530

    Create NPCData.java

---
+++ src/main/java/dev/allenalt/naltnpc/NPCData.java
@@ -0,0 +1,77 @@
+package dev.allenalt.naltnpc;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+
+public class NPCData {
+    
+    private final String id;
+    private final EntityType type;
+    private String name;
+    private Entity entity;
+    private Location location;
+    private Entity hologram;
+    private boolean lookAtPlayers;
+
+    public NPCData(String id, EntityType type, String name, Entity entity, Location location) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.entity = entity;
+        this.location = location;
+        this.hologram = null;
+        this.lookAtPlayers = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public EntityType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        if (entity != null) {
+            entity.setCustomName(name);
+        }
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public void setEntity(Entity entity) {
+        this.entity = entity;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Entity getHologram() {
+        return hologram;
+    }
+
+    public void setHologram(Entity hologram) {
+        this.hologram = hologram;
+    }
+
+    public boolean isLookAtPlayers() {
+        return lookAtPlayers;
+    }
+
+    public void setLookAtPlayers(boolean lookAtPlayers) {
+        this.lookAtPlayers = lookAtPlayers;
+    }
+}
